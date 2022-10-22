3ce72dda1d426e3beff1c1783b8ab24e31ed27f0
Doug Perez <perezcpt@gmail.com>
2022-10-22 16:02:51 -0700

    fetch working

--- app/components/engine/Scene.jsx
+++ app/components/engine/Scene.jsx
@@ -19,7 +19,7 @@ import { CSSToHex, getMeasurementsFromDimensions } from "utils";
 import { colors, base } from "utils/constants";
 
 import styles from "styles/components/scene";
-import axios from "axios";
+// import axios from "axios";
 
 class Scene extends React.Component {
   state = {
@@ -270,26 +270,33 @@ class Scene extends React.Component {
       this.props;
 
     const decrementCounter = () => {
-      axios
-        .post(
-          "https://6rle9e9qnb.execute-api.us-west-1.amazonaws.com/default/serverlessAppFunction",
-          JSON.stringify({ key1: "json Stringify to the rescue" })
-        )
-        .then((response) => {
-          console.log(response);
-        });
+      return fetch(
+        "https://6rle9e9qnb.execute-api.us-west-1.amazonaws.com/default/serverlessAppFunction",
+        {
+          method: "POST", // or 'PUT'
+        }
+      ).then((response) => response.json());
+
+      // fetch(
+      //   "https://6rle9e9qnb.execute-api.us-west-1.amazonaws.com/default/serverlessAppFunction",
+      //   {
+      //     Method: "POST",
+      //   }
+      // ).then((response) => {
+      //   console.log(response);
+      // });
     };
 
-    const incrementCounter = () => {
-      axios
-        .post(
-          " https://6xo1uwhbg0.execute-api.us-west-1.amazonaws.com/default/serverlessAppFunctionIncrement",
-          JSON.stringify({ key1: "json Stringify to the rescue" })
-        )
-        .then((response) => {
-          console.log(response);
-        });
-    };
+    // const incrementCounter = () => {
+    //   axios
+    //     .post(
+    //       " https://6xo1uwhbg0.execute-api.us-west-1.amazonaws.com/default/serverlessAppFunctionIncrement",
+    //       JSON.stringify({ key1: "json Stringify to the rescue" })
+    //     )
+    //     .then((response) => {
+    //       console.log(response);
+    //     });
+    // };
 
     console.log("MODE", mode);
 
@@ -332,7 +339,7 @@ class Scene extends React.Component {
           this._paintCube(intersect);
         } else if (mode === "delete") {
           this._deleteCube(intersect);
-          incrementCounter();
+          // incrementCounter();
         } else if (mode === "rotate") {
           scene.rollOverBrick.rotate(Math.PI / 2);
           scene.setState({

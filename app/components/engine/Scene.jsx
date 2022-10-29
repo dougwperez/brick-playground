52242fcaf24808af1f04f18f6bae1b4ff0e3d2a2
Doug Perez <perezcpt@gmail.com>
2022-10-29 12:56:22 -0700

    remove delete test

--- app/components/engine/Scene.jsx
+++ app/components/engine/Scene.jsx
@@ -377,7 +377,7 @@ class Scene extends React.Component {
         } else if (mode === "delete") {
           this._deleteCube(intersect);
           incrementCounter();
-          saveDataModel();
+          // saveDataModel();
         } else if (mode === "rotate") {
           scene.rollOverBrick.rotate(Math.PI / 2);
           scene.setState({

5b0bef3b6f78fa07732a4859c36221da017c83a5
Doug Perez <perezcpt@gmail.com>
2022-10-29 13:33:39 -0700

    save for paint and delete brought back

--- app/components/engine/Scene.jsx
+++ app/components/engine/Scene.jsx
@@ -373,11 +373,11 @@ class Scene extends React.Component {
           }
         } else if (mode === "paint") {
           this._paintCube(intersect);
-          // saveDataModel();
+          saveDataModel();
         } else if (mode === "delete") {
           this._deleteCube(intersect);
           incrementCounter();
-          // saveDataModel();
+          saveDataModel();
         } else if (mode === "rotate") {
           scene.rollOverBrick.rotate(Math.PI / 2);
           scene.setState({
